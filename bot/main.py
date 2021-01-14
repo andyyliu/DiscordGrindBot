e73514713e305d6434f541f098ab1a881b5de93b
Andy Liu <andyyliu1@gmail.com>
2021-01-13 22:04:22 -0600

    add comments

--- bot/main.py
+++ bot/main.py
@@ -4,17 +4,16 @@ from dotenv import load_dotenv
 from messages import messages
 from rng import rng
 
-# other classes to use
+# cogs
 # extensions = ["messages", "rng", "economy"]
 
 # client = discord.Client()
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix = '!', case_insensitive = True)
 
 # Initial login
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
 
 # Responses to commands and words
 @client.event
@@ -30,7 +29,7 @@ async def on_message(message):
             await message.channel.send(line)
     await client.process_commands(message)
 
-    #Respond to commands
+    #Respond to commands (need to change to command function instead of messages)
     phrase = rng(message)
     if phrase:
         for line in phrase:
@@ -42,6 +41,7 @@ async def on_member_join(member):
     channel = discord.utils.get(member.guild.text_channels, name = "general")
     await channel.send(f'Hi @{member.name}, welcome to The Skate Park!')
 
+# activating the bot
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client.run(TOKEN)
