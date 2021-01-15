156564cc66bc2c4379f939e005c12225ee3e9f7b
Andy Liu <andyyliu1@gmail.com>
2021-01-15 12:49:34 -0600

    changed to cog class

--- bot/rng.py
+++ bot/rng.py
@@ -1,31 +1,42 @@
-import discord, requests, json, random
+import discord, random
 from words import ball, day
+from discord.ext import commands
 
-def rng(message):
-    username = '@' + message.author.name
+class RngCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    # Return a list of commands
-    if message.content.startswith('!help'):
-        phrase = ['A list of currently avaiable commands are: !dice, !coin, !8ball, !day, and !help']
-        return phrase
-
-    # Return a random number from 1-6 when !dice is sent
-    if message.content.startswith('!dice'):
-        phrase = [username + ' takes a deep breath, and rolls the die, the number is.....' + str(random.randint(1, 6)) + '!']
-        return phrase
-
-    # Return heads or tails when !coin is sent
-    if message.content.startswith('!coin'):
-        result = ['heads', 'tails']
-        phrase = [username + ' flips the coin as high as possible, it lands on.....' + result[random.randint(0, len(result)- 1)] + '!']
-        return phrase
+    # Generate a random dice roll (numbers from 1-6)
+    @commands.command()
+    async def dice(self, ctx):
+        username = '@' + ctx.author.mention
+        await ctx.send(username + ' takes a deep breath, and rolls the die, the number is.....' + str(random.randint(1, 6)) + '!')
     
-    # Return an 8ball answer
-    if message.content.startswith('!8ball'):
-        phrase = [username + ' shakes the ball hard, it responds: ' + ball[random.randint(0, len(ball) - 1)]]
-        return phrase
-    
-    # Return prediction of day
-    if message.content == '!day':
-        phrase = [username + ', today will be ' + day[random.randint(0, len(day) - 1)]]
-        return phrase
+    # Generate a random coin flip (heads or tails)
+    @commands.command()
+    async def coin(self, ctx):
+        username = '@' + ctx.author.mention
+        outcomes = ['heads', 'tails', 'its edge, OMG THAT\'S 1 in 6000']
+        rand = random.randint(0, 6000)
+        result = ''
+        if rand == 0:
+            result = outcomes[2]
+        elif rand % 2 == 0:
+            result = outcomes[0]
+        else:
+            result = outcomes[1]
+        await ctx.send(username + ' flips the coin as high as possible, it lands on.....' + result + '!')    
+
+    @commands.command()
+    async def day(self, ctx):
+        username = '@' + ctx.author.mention
+        await ctx.send(username + ', today will be ' + day[random.randint(0, len(day)-1)])
+
+    @commands.command(name='8ball')
+    async def _8ball(self, ctx):
+        username = '@' + ctx.author.mention
+        await ctx.send(username + ' shakes the ball hard, it responds: ' + ball[random.randint(0, len(ball) - 1)])
+
+
+def setup(bot):
+    bot.add_cog(RngCog(bot))
