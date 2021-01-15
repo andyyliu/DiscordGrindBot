3da3959df9d06b4ab1e63bcdf07ae9b2f469e5fb
Andy Liu <andyyliu1@gmail.com>
2021-01-15 11:48:37 -0600

    adding cogs function

--- bot/main.py
+++ bot/main.py
@@ -1,24 +1,29 @@
-import discord, requests, json, random, os
+import discord, requests, json, os, sys, traceback
 from discord.ext import commands
 from dotenv import load_dotenv
 from messages import messages
 from rng import rng
 
 # cogs
-# extensions = ["messages", "rng", "economy"]
+extensions = ["messages", "rng", "economy"]
 
-# client = discord.Client()
-client = commands.Bot(command_prefix = '!', case_insensitive = True)
+bot = commands.Bot(command_prefix = '!', case_insensitive = True)
+
+# if __name__ == '__main__':
+#     for ext in extensions:
+#         bot.load_extension(ext)
 
 # Initial login
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('\n\nLogged in as {0.user}'.format(bot))
+    await bot.change_presence(activity=discord.Game(name="on the grind"))
+    print('Successfully booted!')
 
 # Responses to commands and words
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     message.content = message.content.casefold()
 
@@ -27,16 +32,16 @@ async def on_message(message):
     if phrase:
         for line in phrase:
             await message.channel.send(line)
-    await client.process_commands(message)
+    await bot.process_commands(message)
 
     #Respond to commands (need to change to command function instead of messages)
     phrase = rng(message)
     if phrase:
         for line in phrase:
             await message.channel.send(line)
-    await client.process_commands(message)
+    await bot.process_commands(message)
 
-@client.event
+@bot.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.text_channels, name = "general")
     await channel.send(f'Hi @{member.name}, welcome to The Skate Park!')
@@ -44,4 +49,4 @@ async def on_member_join(member):
 # activating the bot
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-client.run(TOKEN)
+bot.run(TOKEN, bot = True, reconnect = True)
