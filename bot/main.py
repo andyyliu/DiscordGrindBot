e9c9f13f08543c0f2927c7592f2a8704cc4955fe
Andy Liu <andyyliu1@gmail.com>
2021-01-15 14:36:23 -0600

    changes

--- bot/main.py
+++ bot/main.py
@@ -2,8 +2,9 @@ import discord, requests, json, os, sys, traceback
 from discord.ext import commands
 from dotenv import load_dotenv
 
+
 # cogs
-extensions = ['rng', 'messages']
+extensions = ['rng', 'messages', 'date']
 
 bot = commands.Bot(command_prefix = '!', case_insensitive = True)
 
@@ -18,7 +19,7 @@ async def on_ready():
     print('\n\nLogged in as {0.user}'.format(bot))
     await bot.change_presence(activity=discord.Game(name="on the grind"))
     print('Successfully booted!')
-
+    
 
 # activating the bot
 load_dotenv()
--- bot/rng.py
+++ bot/rng.py
@@ -3,11 +3,9 @@ from words import ball, day
 from discord.ext import commands
 from messages import get_quote
 
-
 class Decisions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
 
     # Generate a random dice roll (numbers from 1-6)
     @commands.command()
@@ -15,7 +13,6 @@ class Decisions(commands.Cog):
         username = ctx.author.mention
         await ctx.send(username + ' takes a deep breath, and rolls the die, the number is.....' + str(random.randint(1, 6)) + '!')
     
-
     # Generate a random coin flip (heads, tails, or edge(~1/6000))
     @commands.command()
     async def coin(self, ctx):
@@ -31,20 +28,17 @@ class Decisions(commands.Cog):
             result = outcomes[1]
         await ctx.send(username + ' flips the coin as high as possible, it lands on.....' + result + '!')    
 
-
     # Generate a random forecast of user's day
     @commands.command()
     async def day(self, ctx):
         username = ctx.author.mention
         await ctx.send(username + ', today will be ' + day[random.randint(0, len(day)-1)])
 
-
     # Generate a random 8ball answer
     @commands.command(name='8ball')
     async def _8ball(self, ctx):
         username = ctx.author.mention
         await ctx.send(username + ' shakes the ball hard, it responds: ' + ball[random.randint(0, len(ball) - 1)])
-
 
     # Generate a random motivational quote
     @commands.command()
